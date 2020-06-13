15cbce03d6ba9c8865f00b5429e1a1579d02d79f
P-JSL <64994827+P-JSL@users.noreply.github.com>
2020-06-13 10:02:23 +0900

    시퀸스 쿼리 파일

---
+++ seqn.sql
@@ -0,0 +1,48 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 토요일-6월-13-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence SEQ_REVIEW
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_REVIEW"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_RESTABLE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_RESTABLE"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_RES
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_RES"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_QNA
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_QNA"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_PRODUCT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_PRODUCT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_NOTICEWRITE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_NOTICEWRITE"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_APPLY
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_APPLY"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CONT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_CONT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_N_REPLY
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_N_REPLY"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
