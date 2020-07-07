371babeed4f071c1a603324ba71b9ecddf391a9c
P-JSL <64994827+P-JSL@users.noreply.github.com>
2020-07-07 19:08:05 +0900

    Add files via upload

---
+++ SqlFile/AllTableSeq.sql
@@ -0,0 +1,68 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-7월-07-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence SEQ_NOTICEWRITE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_NOTICEWRITE"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_RESTABLE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_RESTABLE"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_PRODUCT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_PRODUCT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_AR
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_AR"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CUSER
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_CUSER"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_QNA
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_QNA"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_REVIEW
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_REVIEW"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_N_REPLY
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_N_REPLY"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 123 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_APPLY
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_APPLY"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CART
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_CART"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_RES
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_RES"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 62 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_USERNOTICEWRITE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_USERNOTICEWRITE"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CONT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SEQ_CONT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE
